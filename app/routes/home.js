4fae89e221f0f449a453f17c1a5e11e8680e6643
Christophe Karam <christophe.karam@grenoble-inp.org>
2022-07-03 19:37:57 +0200

    Add static build hosting in router

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,14 +2,26 @@ const path = require('path')
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.send("Home Router Test");
-});
+router.get('/', async (req, res, next) => {
+  res.sendFile(
+      path.join(__dirname, '../client', 'index.html'))
+})
 
-router.use('/index', express.static(path.join('public', 'index.html')));
+router.use(express.static(path.join(__dirname, '../client')))
 
-router.use('/css', express.static('public/css'));
-router.use('/js', express.static('public/js'));
-router.use('/images', express.static('public/images'));
+router.get('/*', async (req, res, next) => {
+  res.sendFile(
+      path.join(__dirname, '../client', 'index.html'))
+})
+
+// router.use('/index', express.static(path.join('public', 'index.html')));
+// router.use('/css', express.static('public/css'));
+// router.use('/js', express.static('public/js'));
+//
+// router.use('/g',  express.static(path.join('build', 'index.html')));
+// router.use('/static/css', express.static('build/static/css'));
+// router.use('/static/js', express.static('build/static/js'));
+
+
 
 module.exports = router;
