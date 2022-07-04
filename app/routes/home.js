7a8872b66c603057a7cd00a52244f01b56398ab5
Christophe Karam <christophe.karam@grenoble-inp.org>
2022-07-04 13:01:21 +0200

    Update routes with video redirect

--- app/routes/home.js
+++ app/routes/home.js
@@ -7,21 +7,13 @@ router.get('/', async (req, res, next) => {
       path.join(__dirname, '../client', 'index.html'))
 })
 
+router.get('/hackaton-video', async (req, res, next) => {
+  res.redirect('https://www.youtube.com/watch?v=wpdETyTx5Ak');
+})
+
 router.use(express.static(path.join(__dirname, '../client')))
 
 router.get('/*', async (req, res, next) => {
   res.sendFile(
       path.join(__dirname, '../client', 'index.html'))
 })
-
-// router.use('/index', express.static(path.join('public', 'index.html')));
-// router.use('/css', express.static('public/css'));
-// router.use('/js', express.static('public/js'));
-//
-// router.use('/g',  express.static(path.join('build', 'index.html')));
-// router.use('/static/css', express.static('build/static/css'));
-// router.use('/static/js', express.static('build/static/js'));
-
-
-
-module.exports = router;
